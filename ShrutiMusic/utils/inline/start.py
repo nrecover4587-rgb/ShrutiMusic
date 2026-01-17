fc16d387fc8aa9d8e86fe557563544bfbd90c80d
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-17 20:04:20 +0530

    Update start.py

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -57,9 +57,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(
-                text=_[""],
-                callback_data="fork_repo"
+            InlineKeyboardButton("
             ),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
