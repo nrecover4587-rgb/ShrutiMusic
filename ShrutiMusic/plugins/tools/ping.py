f67b66fc598459663ec8a55575c56419cb9e70d3
nrecover4587-rgb <nrecover4587@gmail.com>
2026-01-18 20:34:54 +0530

    Update ping.py

--- ShrutiMusic/plugins/tools/ping.py
+++ ShrutiMusic/plugins/tools/ping.py
@@ -38,7 +38,7 @@ from config import BANNED_USERS, PING_IMG_URL
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_photo(
-        photo=PING_IMG_URL,
+        photo=https://files.catbox.moe/x8dhjp.jpg,
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await Nand.ping()
